767890d87272de9bfaa320956686ca3df9256286
Abdoulaye BARRY <contact@abdoulayebarry.com>
2014-09-15 11:51:02 +0200

    AXAWHAPI-32 Removes unused code

--- src/Axa/Bundle/WhapiBundle/Controller/PlatformController.php
+++ src/Axa/Bundle/WhapiBundle/Controller/PlatformController.php
@@ -3,18 +3,11 @@
 namespace Axa\Bundle\WhapiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 class PlatformController extends Controller
 {
     public function createAction()
     {
-        $data = $this->getRequest()->request->all();
-        $platformService = $this->get('axa_whapi.platform');
-        $platformService->create($data['userEmail'], $data['offerCode']);
-
-        return new JsonResponse($data);
-
 
     }
 
