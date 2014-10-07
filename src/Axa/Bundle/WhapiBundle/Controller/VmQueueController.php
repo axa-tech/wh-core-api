e4e1dd6890cd500a506f75c0bef5589fa029c2eb
Abdoulaye BARRY <contact@abdoulayebarry.com>
2014-10-07 18:01:03 +0200

    AXAWHAPI-89 removes unused code

--- src/Axa/Bundle/WhapiBundle/Controller/VmQueueController.php
+++ src/Axa/Bundle/WhapiBundle/Controller/VmQueueController.php
@@ -2,19 +2,10 @@
 
 namespace Axa\Bundle\WhapiBundle\Controller;
 
-use Axa\Bundle\WhapiBundle\Exception\PlatformNotFoundException;
-use Axa\Bundle\WhapiBundle\Exception\RuntimeException;
 use FOS\RestBundle\Controller\FOSRestController,
-    FOS\RestBundle\View\View,
     FOS\RestBundle\Controller\Annotations as Rest;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
-
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class VmQueueController extends FOSRestController
 {
